36848781a8c866438ad8a9209bf9f976f5156d6e
dijunliu <362172145@qq.com>
2019-09-30 11:18:11 +0800

    add header

---
+++ src/pages/home/components/HomeHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe658;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe63c;</span>输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe659;</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HomeHeader"
+};
+</script>
+<style lang='stylus' scoped>
+@import '~style/varibles.styl';
+
+.header {
+  line-height: 0.96rem;
+  display: flex;
+  background: $bgColor;
+  color: #fff;
+
+  .header-left {
+    float: left;
+    width: 0.64rem;
+    text-align: center;
+
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-right {
+    float: rigth;
+    width: 1.24rem;
+    text-align: center;
+  }
+
+  .header-input {
+    float: rigth;
+    width: 1.24rem;
+    flex: 1;
+    background: #fff;
+    border-radius: 4px;
+    margin: 8px 0;
+    height: 0.67rem;
+    line-height: 0.6rem;
+    color: #a6a8a9;
+    padding-left: 5px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home'
+import List from '@/pages/list'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List
     }
   ]
 })
